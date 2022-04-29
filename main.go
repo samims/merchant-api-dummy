5ed61935c0361adeb6395f45c561076e3878e277
Samiul Sk <samiul@rely.sg>
2022-04-30 00:03:35 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+	logrus.Info("Server listening on port 3000")
+
+	http.ListenAndServe(":3000", r)
+}
