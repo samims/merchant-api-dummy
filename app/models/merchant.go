a328cd87be419d3c37eb10aa89d81a48f6ed661a
Samiul Sk <samiul@rely.sg>
2022-05-04 21:17:40 +0530

    modify: User field to Team

--- app/models/merchant.go
+++ app/models/merchant.go
@@ -4,10 +4,10 @@ import "github.com/astaxie/beego/orm"
 
 type Merchant struct {
 	BaseModel
-	Name  string  `json:"name" orm:"column(name)"`
-	URL   string  `json:"url" orm:"column(url)"`
-	Code  string  `json:"code" orm:"column(code); unique"`
-	Users []*User `json:"users" orm:"reverse(many)"`
+	Name string  `json:"name" orm:"column(name)"`
+	URL  string  `json:"url" orm:"column(url)"`
+	Code string  `json:"code" orm:"column(code); unique"`
+	Team []*User `json:"teams" orm:"reverse(many)"`
 }
 
 func (m *Merchant) TableName() string {
