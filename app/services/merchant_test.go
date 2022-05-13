85ee887ff1dd97fbb1ee49bc3ef3263386d3f686
Samiul Sk <samiul@rely.sg>
2022-05-13 14:02:08 +0530

    test: add test for merchant service upto update

--- app/services/merchant_test.go
+++ app/services/merchant_test.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/astaxie/beego/orm"
@@ -33,7 +34,7 @@ func (suite *MerchantServiceTestSuite) SetupTest() {
 	suite.SUT = NewMerchantService(suite.mockMerchantRepo, suite.userRepoMock)
 }
 
-func (suite *MerchantServiceTestSuite) TestVerifyObjectPermissionTestRaisesErrorIfUserDoesNotExist() {
+func (suite *MerchantServiceTestSuite) TestMerchantServiceVerifyObjectPermissionTestRaisesErrorIfUserDoesNotExist() {
 	// Arrange
 	user := models.User{
 		BaseModel: models.BaseModel{
@@ -57,7 +58,9 @@ func (suite *MerchantServiceTestSuite) TestVerifyObjectPermissionTestRaisesError
 	suite.userRepoMock.AssertExpectations(suite.T())
 }
 
-func (suite *MerchantServiceTestSuite) TestVerifyObjectPermissionTestRaisesErrorIfMerchantDoesNotExist() {
+// TestVerifyObjectPermissionTestRaisesErrorIfMerchantDoesNotExist is a unit test for MerchantService.VerifyObjectPermission
+// It verifies that the VerifyObjectPermission method raises an error if merchant's user is not same as current user
+func (suite *MerchantServiceTestSuite) TestMerchantServiceVerifyObjectPermissionTestRaisesErrorIfMerchantDoesNotExist() {
 	// Arrange
 	merchant := models.Merchant{
 		BaseModel: models.BaseModel{
@@ -86,7 +89,9 @@ func (suite *MerchantServiceTestSuite) TestVerifyObjectPermissionTestRaisesError
 
 }
 
-func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUserDoesnotExist() {
+// TestMerchantCreateRaisesIfUserDoesnotExist is a unit test for MerchantService.Create
+// It verifies that the Create method raises an error if the user does not exist
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateRaisesIfUserDoesnotExist() {
 	// Arrange
 	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
 	merchant := models.Merchant{
@@ -108,7 +113,9 @@ func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUserDoesnotExis
 	suite.userRepoMock.AssertExpectations(suite.T())
 }
 
-func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUserAlreadyAttachedToMerchant() {
+// TestMerchantCreateRaisesIfUserAlreadyAttachedToMerchant is a unit test for MerchantService.Create
+// It verifies that the Create method raises an error if the user is already attached to a merchant
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateRaisesIfUserAlreadyAttachedToMerchant() {
 	// Arrange
 	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
 	merchant := models.Merchant{
@@ -134,7 +141,10 @@ func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUserAlreadyAtta
 	suite.userRepoMock.AssertExpectations(suite.T())
 }
 
-func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfWriteToDBFailed() {
+// TestMerchantCreateRaisesIfMerchantDataWriteToDBFailed is a unit test for MerchantService.Create
+// It verifies that the Create method raises an error if the merchant data is not written to the DB
+// due to an error in the DB layer
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateRaisesIfMerchantDataWriteToDBFailed() {
 	// Arrange
 	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
 	merchant := models.Merchant{
@@ -163,7 +173,9 @@ func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfWriteToDBFailed
 
 }
 
-func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUpdateUserAfterMerchantSaveReturnsError() {
+// TestMerchantCreateRaisesIfUpdateUserAfterMerchantSaveReturnsError tests if user attachment with merchant
+// fails it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateRaisesIfUpdateUserAfterMerchantSaveReturnsError() {
 	// Arrange
 	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
 	merchant := models.Merchant{
@@ -190,6 +202,247 @@ func (suite *MerchantServiceTestSuite) TestMerchantCreateRaisesIfUpdateUserAfter
 	suite.Equal(orm.ErrStmtClosed.Error(), err.Error())
 	suite.Empty(result)
 
+}
+
+// TestMerchantCreateRaisesIfMerchantRollbackFails test if merchant creation is faild due to
+// the cause that the user already has a merchant and then rollback is faild then it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateRaisesIfMerchantRollbackFails() {
+	// Arrange
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
+	merchant := models.Merchant{
+		Name: fake.Company(),
+		URL:  fake.DomainName(),
+	}
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+	suite.mockMerchantRepo.On("Save", ctx, mock.Anything).Return(nil)
+	suite.userRepoMock.On("Update", ctx, mock.Anything, mock.Anything).Return(orm.ErrStmtClosed)
+	suite.mockMerchantRepo.On("Delete", ctx, mock.Anything).Return(orm.ErrArgs)
+
+	// Act
+	result, err := suite.SUT.Create(ctx, merchant)
+
+	// Assert
+	suite.Error(err)
+	suite.Equal(orm.ErrArgs.Error(), err.Error())
+	suite.Empty(result)
+
+}
+
+// TestMerchantCreateSuccess tests if merchant creation is successful
+// it should return PublicMerchant object and no error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceCreateSuccess() {
+	// Arrange
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, int64(1))
+	merchant := models.Merchant{
+		Name: fake.Company(),
+		URL:  fake.DomainName(),
+	}
+
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+	suite.mockMerchantRepo.On("Save", ctx, mock.Anything).Return(nil)
+	suite.userRepoMock.On("Update", ctx, mock.Anything, mock.Anything).Return(nil)
+
+	// Act
+	result, err := suite.SUT.Create(ctx, merchant)
+
+	// Assert
+	suite.NoError(err)
+	// assert that result is models.PublicMerchant
+	suite.Equal(reflect.TypeOf(models.PublicMerchant{}), reflect.TypeOf(result))
+	suite.Equal(merchant.Name, result.Name)
+
+	suite.userRepoMock.AssertExpectations(suite.T())
+}
+
+// TestMerchantGetRaisesIfUserNotFound tests if user is not found then it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceGetRaisesIfUserNotFound() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	// Mock
+	suite.mockMerchantRepo.On("FindOne", suite.ctx, mock.Anything).Return(&merchant, orm.ErrNoRows)
+
+	// Act
+	result, err := suite.SUT.Get(suite.ctx, merchant.Id)
+
+	// Assert
+	suite.Error(err)
+	suite.Equal(orm.ErrNoRows.Error(), err.Error())
+	suite.Equal(models.PublicMerchant{}, result)
+	suite.Empty(result)
+
+}
+
+// TestMerchantGetSuccess tests if merchant is found then it should return PublicMerchant object and no error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceGetSuccess() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	// Mock
+	suite.mockMerchantRepo.On("FindOne", suite.ctx, mock.Anything).Return(&merchant, nil)
+
+	// Act
+	result, err := suite.SUT.Get(suite.ctx, merchant.Id)
+
+	// Assert
+	suite.NoError(err)
+	suite.Equal(reflect.TypeOf(models.PublicMerchant{}), reflect.TypeOf(result))
+
+}
+
+// TestMerchantServiceUpdateRaisesIfUserDoNotHavePermission tests if user do not have permission to update merchant
+// then it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceUpdateRaisesIfUserDoNotHavePermission() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+		Merchant: &merchant,
+	}
+
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, user.Id)
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+
+	// Act
+	result, err := suite.SUT.Update(ctx, merchant.Id+1, merchant)
+
+	// Assert
+	suite.Error(err)
+	suite.Equal(constants.PermissionDenied, err.Error())
+	suite.Equal(models.PublicMerchant{}, result)
+
+}
+
+// TestMerchantServiceUpdateRaisesIfMerchantNotFound tests if merchant is not found then it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceUpdateRaisesIfMerchantNotFound() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+		Merchant: &merchant,
+	}
+
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, user.Id)
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+	suite.mockMerchantRepo.On("FindOne", ctx, mock.Anything).Return(nil, orm.ErrNoRows)
+
+	// Act
+	result, err := suite.SUT.Update(ctx, merchant.Id, merchant)
+
+	// Assert
+	suite.Error(err)
+	suite.Equal(orm.ErrNoRows.Error(), err.Error())
+	suite.Equal(models.PublicMerchant{}, result)
+
+	suite.userRepoMock.AssertExpectations(suite.T())
+
+}
+
+// TestMerchantServiceUpdateRaisesIfUpdateQueryFails tests if update query fails then it should return error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceUpdateRaisesIfUpdateQueryFails() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+		Merchant: &merchant,
+	}
+
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, user.Id)
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+	suite.mockMerchantRepo.On("FindOne", ctx, mock.Anything).Return(&merchant, nil)
+	suite.mockMerchantRepo.On("Update", ctx, mock.Anything, mock.Anything).Return(orm.ErrStmtClosed)
+
+	// Act
+	result, err := suite.SUT.Update(ctx, merchant.Id, merchant)
+
+	// Assert
+	suite.Error(err)
+	suite.Equal(orm.ErrStmtClosed.Error(), err.Error())
+	suite.Equal(models.PublicMerchant{}, result)
+
+	suite.userRepoMock.AssertExpectations(suite.T())
+	suite.mockMerchantRepo.AssertExpectations(suite.T())
+}
+
+// TestMerchantServiceUpdateSuccess tests if merchant is found then it should return PublicMerchant object and no error
+func (suite *MerchantServiceTestSuite) TestMerchantServiceUpdateSuccess() {
+	// Arrange
+	merchant := models.Merchant{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+	}
+
+	user := models.User{
+		BaseModel: models.BaseModel{
+			Id: int64(1),
+		},
+		Merchant: &merchant,
+	}
+
+	ctx := context.WithValue(suite.ctx, constants.UserIDContextKey, user.Id)
+
+	// Mock
+	suite.userRepoMock.On("FindOne", ctx, mock.Anything).Return(&user, nil)
+	suite.mockMerchantRepo.On("FindOne", ctx, mock.Anything).Return(&merchant, nil)
+	suite.mockMerchantRepo.On("Update", ctx, mock.Anything, mock.Anything).Return(nil)
+
+	// Act
+	result, err := suite.SUT.Update(ctx, merchant.Id, merchant)
+
+	// Assert
+	suite.NoError(err)
+	suite.Equal(reflect.TypeOf(models.PublicMerchant{}), reflect.TypeOf(result))
+
+	suite.userRepoMock.AssertExpectations(suite.T())
+	suite.mockMerchantRepo.AssertExpectations(suite.T())
 }
 
 func TestMerchantService(t *testing.T) {
